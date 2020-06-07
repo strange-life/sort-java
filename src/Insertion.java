92c385e2f8f13001697455a2e9c65ec2cc6b838a
strange-life <strange.hao@gmail.com>
2020-06-07 17:23:51 +0800

    add insertion sort

---
+++ src/Insertion.java
@@ -0,0 +1,21 @@
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Insertion<T extends Comparable<T>> extends Sort<T> {
+    public void sort(T[] a) {
+        for (int i = 1; i < a.length; i++)
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--)
+                exch(a, j, j - 1);
+    }
+
+    public static void main(String[] args) {
+        Insertion<Double> insertion = new Insertion<>();
+        Double[] a = new Double[10];
+
+        for (int i = 0; i < a.length; i++)
+            a[i] = StdRandom.uniform();
+
+        insertion.sort(a);
+        assert insertion.isSorted(a);
+        insertion.show(a);
+    }
+}
